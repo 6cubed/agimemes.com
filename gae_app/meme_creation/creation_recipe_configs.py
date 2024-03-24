545e6d9a994457217f08c9400b4454162b1a01ff
216 <youremail@example.com>
2024-03-24 22:38:47 +0100

    add more news sources

--- gae_app/meme_creation/creation_recipe_configs.py
+++ gae_app/meme_creation/creation_recipe_configs.py
@@ -1,5 +1,5 @@
 BASE_RECIPE = {
-  "news_source": ['newsapi_techcrunch', 'newsapi_ai'],
+  "news_source": ['newsapi_techcrunch', 'newsapi_ai', 'newsapi_cnn', 'newsapi_biuk', 'newsapi_rte', 'newsapi_wsj'],
   "llm": ['chatgpt', 'mistral', 'neets.ai'],
   "personality": None,
 }
